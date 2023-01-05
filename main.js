c946dc85b6572f651fa54fb4e0dbad3dbcd91aeb
shuluke1030 <shuluke1030@gmail.com>
2023-01-05 21:41:20 +0800

    Finish

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function countPositivesSumNegatives(input) {
+    if (input === null || input.length === 0){
+      return []
+   }
     let count = 0;
     let sum = 0;
 
@@ -11,4 +14,3 @@ function countPositivesSumNegatives(input) {
     }
     return [count , sum]
   }
-    
